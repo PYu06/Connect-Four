5acc0b10e2f080af2d9f9e460a9aa96198550bcb
CJ Johnson & Kevin Xu <pair+fermentedbeats.midnightapplepie@devbootcamp.com>
2014-12-18 19:59:47 -0800

    almost

--- JS.js
+++ JS.js
@@ -22,14 +22,40 @@ window.onload = function () {
     cell.style.top="0";
   }
 
+
   var player1={
-    img:'./image/stewie.png',
+    img:'',
     name:"player1"
   }
 
   var player2={
-    img:'./image/brian.png',
+    img:'',
     name:"player2"
+  }
+
+
+  var assignChar=function(){
+    var characters=document.querySelector("#characters");
+    characters.addEventListener("click",grapPic);
+  }
+
+  var grapPic=function(e){
+   if (player1.img===""){
+    player1.img="./image/"+ e.target.id +".png"
+  }else if (player2.img===""){
+    player2.img="./image/" + e.target.id + ".png"
+  }
+  }
+
+  var charactersText=function(){
+    var text=document.querySelector("#characters > p")
+    if (player1.img===""){
+      text.innerHTML="Choose character for Player 1";
+    }else if (player2.img===""){
+      text.innerHTML="Choose character for Player 2";
+    }else{
+      text.innerHTML="Game Begins!!!";
+    }
   }
 
   var activePlayer={
@@ -45,6 +71,12 @@ window.onload = function () {
     }
   }
 
+   var dropCell=function(id){
+    var cell = document.getElementById(id)
+    cell.innerHTML='<img src='+ activePlayer.currentPlayer().img + '>';
+    cell.style.top="0";
+  }
+
     var findEmptyCell = function() {
     $( '[class^=column]' ).click(function() {
       var emptyCell = $(this).find(".empty").last()
@@ -54,12 +86,11 @@ window.onload = function () {
   });
   }
 
-    var getCord()=function{
-      document.querySelectorAll("."+activePlayer)
-    }
 
 
   createColumns();
   createID();
   findEmptyCell();
+  charactersText();
+  assignChar();
 }
